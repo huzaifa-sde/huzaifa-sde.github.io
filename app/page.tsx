1dd22096385703980969a811cd3ca472c14b541e
Huzaifa-Ali-Khan <huzaifalikhan48@gmail.com>
2025-07-06 21:34:05 +0500

    Remove debug log for selected project in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,6 @@ export default function Page() {
   const [whatsappModalOpen, setWhatsappModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-  console.log("Selected Project:", selectedProject);
   const scrollToContact = () => {
     const contactSection = document.getElementById("contact");
     if (contactSection) {
